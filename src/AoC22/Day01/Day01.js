c49d93786bb43b83bb16cd3bacbb06e90c0c489b
Kelsey Morse-Brown <kelsey@jellyvision.com>
2022-12-01 13:13:17 -0600

    2022 Day 1 solution

--- src/AoC22/Day01/Day01.js
+++ src/AoC22/Day01/Day01.js
@@ -1,22 +1,59 @@
+const fs = require('fs');
 const path = require('path');
 const { getData } = require(path.join(
   path.dirname(path.dirname(__dirname)),
   'globalFunctions.js'
 ));
 
-async function Day01Function(filepath) {
+console.log(
+  path.join(path.dirname(path.dirname(__dirname)), 'globalFunctions.js')
+);
+
+async function formatData(filepath) {
   const data = await getData(filepath);
-  return data;
+  const arrayOfStrings = data.split('\n\n').filter(String);
+  let formattedData = [];
+
+  arrayOfStrings.forEach((element) => {
+    const stringArray = element.split('\n');
+    const numberArray = stringArray.map(Number);
+    formattedData.push(numberArray);
+  });
+
+  return formattedData;
+}
+async function sumCalories(input) {
+  let sumArray = [];
+  input.forEach((element) => {
+    sumArray.push(element.reduce((a, b) => a + b, 0));
+  });
+
+  return sumArray;
+}
+async function getHighestTotal(input) {
+  return Math.max(...input);
 }
 
-async function runDay01() {
-  const filepath = path.join(__dirname, 'Day01Input.txt');
-  const result = await Day01Function(filepath);
-  console.log(result);
+async function runDay01(input) {
+  try {
+    const filepath = input;
+    const formattedData = await formatData(filepath);
+    const calloriesSumArray = await sumCalories(formattedData);
+    const highestTotalCalories = await getHighestTotal(calloriesSumArray);
+    await fs.promises.writeFile(
+      path.join(__dirname, 'Day01Result.txt'),
+      JSON.stringify(highestTotalCalories)
+    );
+    return highestTotalCalories;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-runDay01();
+runDay01(path.join(__dirname, 'Day01Input.txt'));
 
 module.exports = {
-  Day01Function,
+  getHighestTotal,
+  formatData,
+  sumCalories,
 };
--- src/AoC22/Day01/Day01.spec.js
+++ src/AoC22/Day01/Day01.spec.js
@@ -1,11 +1,37 @@
 const path = require('path');
-const { Day01Function } = require('./Day01.js');
+const { formatData, sumCalories, getHighestTotal } = require('./Day01.js');
 
 describe('Day01', () => {
-  describe('Day01Function', () => {
-    it('Finds the total calories carried by the elf carrying the most calories', async () => {
+  describe('formatData', () => {
+    it('Formats the data into an array of arrays', async () => {
       const args = path.join(__dirname, 'Day01TestData.txt');
-      const actual = await getData(args);
+      const actual = await formatData(args);
+      expect(actual).toEqual([
+        [1000, 2000, 3000],
+        [4000],
+        [5000, 6000],
+        [7000, 8000, 9000],
+        [10000],
+      ]);
+    });
+  });
+  describe('sumCalories', () => {
+    it('Sums each array', async () => {
+      const args = [
+        [1000, 2000, 3000],
+        [4000],
+        [5000, 6000],
+        [7000, 8000, 9000],
+        [10000],
+      ];
+      const actual = await sumCalories(args);
+      expect(actual).toEqual([6000, 4000, 11000, 24000, 10000]);
+    });
+  });
+  describe('getHighestTotal', () => {
+    it('Finds the total calories carried by the elf carrying the most calories', async () => {
+      const args = [6000, 4000, 11000, 24000, 10000];
+      const actual = await getHighestTotal(args);
       expect(actual).toEqual(24000);
     });
   });
