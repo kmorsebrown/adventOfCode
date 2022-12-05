33b8f88e4ceee974a164eb79ef28983b3c22de24
Kelsey Morse-Brown <kelsey@jellyvision.com>
2022-12-04 20:37:35 -0600

    Day 4 solution

---
+++ src/AoC22/Day04/Day04.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const path = require('path');
+const { getData } = require(path.join(
+  path.dirname(path.dirname(__dirname)),
+  'globalFunctions.js'
+));
+
+async function formatData(filepath) {
+  const data = await getData(filepath);
+  const arrayOfStrings = data.split('\n').filter(String);
+  const arrayOfarray = arrayOfStrings.map((x) => {
+    return x.split(/[\W/]+/);
+  });
+  return arrayOfarray;
+}
+
+// Part One
+
+async function getCountOfFullyContainedOverlaps(input) {
+  let fullyContainsCount = 0;
+
+  input.forEach((pair) => {
+    const elf1Start = Number(pair[0]);
+    const elf1End = Number(pair[1]);
+    const elf2Start = Number(pair[2]);
+    const elf2End = Number(pair[3]);
+
+    if (elf1Start >= elf2Start && elf1End <= elf2End) {
+      fullyContainsCount++;
+    } else if (elf2Start >= elf1Start && elf2End <= elf1End) {
+      fullyContainsCount++;
+    }
+  });
+  return fullyContainsCount;
+}
+
+// Part Two
+async function getNumOverlaps(input) {
+  let overlapsCount = 0;
+
+  input.forEach((pair) => {
+    const elf1Start = Number(pair[0]);
+    const elf1End = Number(pair[1]);
+    const elf2Start = Number(pair[2]);
+    const elf2End = Number(pair[3]);
+
+    if (elf1Start >= elf2Start && elf1Start <= elf2End) {
+      overlapsCount++;
+    } else if (elf1End <= elf2End && elf1End >= elf2Start) {
+      overlapsCount++;
+    } else if (elf2Start >= elf1Start && elf2Start <= elf1End) {
+      overlapsCount++;
+    } else if (elf2End <= elf1End && elf2End >= elf1Start) {
+      overlapsCount++;
+    }
+  });
+  return overlapsCount;
+}
+
+async function runDay04() {
+  const dataPath = path.join(__dirname, 'Day04Input.txt');
+
+  try {
+    const formattedData = await formatData(dataPath);
+    const results = await Promise.all([
+      await getCountOfFullyContainedOverlaps(formattedData),
+      await getNumOverlaps(formattedData),
+    ]);
+    return results;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = {
+  formatData,
+  getCountOfFullyContainedOverlaps,
+  getNumOverlaps,
+  runDay04,
+};
---
+++ src/AoC22/Day04/Day04.spec.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const {
+  formatData,
+  getCountOfFullyContainedOverlaps,
+  getNumOverlaps,
+} = require('./Day04.js');
+
+describe('Day04', () => {
+  describe('formatData', () => {
+    it('Formats the data into an array', async () => {
+      const args = path.join(__dirname, 'Day04TestData.txt');
+      const actual = await formatData(args);
+      expect(actual).toEqual([
+        ['2', '4', '6', '8'],
+        ['2', '3', '4', '5'],
+        ['5', '7', '7', '9'],
+        ['2', '8', '3', '7'],
+        ['6', '6', '4', '6'],
+        ['2', '6', '4', '8'],
+      ]);
+    });
+  });
+  describe('getCountOfFullyContainedOverlaps', () => {
+    it('Finds the number of pairs where one range is fully contained within the other', async () => {
+      const args = [
+        ['20', '40', '60', '80'],
+        ['2', '3', '4', '5'],
+        ['5', '7', '7', '9'],
+        ['2', '8', '3', '7'],
+        ['6', '6', '4', '6'],
+        ['2', '6', '4', '8'],
+      ];
+      const actual = await getCountOfFullyContainedOverlaps(args);
+      expect(actual).toEqual(2);
+    });
+  });
+  describe('getNumOverlaps', () => {
+    it('Finds the number of pairs with sections that overlap', async () => {
+      const args = [
+        ['2', '4', '6', '8'],
+        ['2', '3', '4', '5'],
+        ['5', '7', '7', '9'],
+        ['2', '8', '3', '7'],
+        ['6', '6', '4', '6'],
+        ['2', '6', '4', '8'],
+      ];
+      const actual = await getNumOverlaps(args);
+      expect(actual).toEqual(4);
+    });
+  });
+});
