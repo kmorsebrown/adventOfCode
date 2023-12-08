d4d30875257c562eaf55afd934de666c4a8db510
Kelsey Morse-Brown <132404623+kelseymorsebrown@users.noreply.github.com>
2023-12-07 21:31:24 -0600

    Aoc 2023 day7 (#25)
    2023 Day 7 solve

---
+++ src/AoC23/Day07/Day07.js
@@ -0,0 +1,315 @@
+const { getData } = require('../../Utils/globalFunctions.js');
+const { sum } = require('../../Utils/maths.js');
+
+// https://adventofcode.com/2023/day/7
+exports.formatData = async (filepath) => {
+  let data = await getData(filepath);
+
+  data = data.split('\n').map((e) => {
+    let [hand, bid] = e.split(' ');
+    bid = parseInt(bid, 10);
+    return {
+      hand: hand,
+      bid: bid,
+    };
+  });
+  return data;
+};
+
+// Hand Types
+const FIVE_OF_A_KIND = 'FIVE_OF_A_KIND'; // all 5 share label
+const FOUR_OF_A_KIND = 'FOUR_OF_A_KIND'; // 4 share label, 1 w/distinct label
+const FULL_HOUSE = 'FULL_HOUSE'; // 3 share label, 2 share 2nd label
+const THREE_OF_A_KIND = 'THREE_OF_A_KIND'; // 3 share label, 2 w/distinct labels
+const TWO_PAIR = 'TWO_PAIR'; // 2 share label, 2 share 2nd label
+const ONE_PAIR = 'ONE_PAIR'; // 2 share label, 3 w/distinct labels
+const HIGH_CARD = 'HIGH_CARD'; // all 5 have distinct labels
+
+// Part One
+
+// Gets unique characters in a string
+const getDistinct = (str) => {
+  let s = new Set();
+  for (const char of str) {
+    s.add(char);
+  }
+  return s;
+};
+
+// Counts instances of characters in a string
+const countOccurrence = (char, str) => {
+  return str.match(new RegExp(char, 'g')).length;
+};
+
+exports.getHandType = (hand) => {
+  const distinctChars = getDistinct(hand);
+  let charOccurrences = [];
+
+  switch (distinctChars.size) {
+    case 1:
+      return FIVE_OF_A_KIND;
+    case 2:
+      for (const char of distinctChars) {
+        charOccurrences.push(countOccurrence(char, hand));
+      }
+      return charOccurrences.includes(4) ? FOUR_OF_A_KIND : FULL_HOUSE;
+    case 3:
+      for (const char of distinctChars) {
+        charOccurrences.push(countOccurrence(char, hand));
+      }
+      return charOccurrences.includes(3) ? THREE_OF_A_KIND : TWO_PAIR;
+    case 4:
+      return ONE_PAIR;
+    case 5:
+      return HIGH_CARD;
+  }
+};
+
+exports.compareHandStrengthPartOne = (a, b) => {
+  // Order hands first by type
+
+  // Hand type, strongest to weakest:
+  const handType = [
+    FIVE_OF_A_KIND, // all 5 share label
+    FOUR_OF_A_KIND, // 4 share label, 1 w/distinct label
+    FULL_HOUSE, // 3 share label, 2 share 2nd label
+    THREE_OF_A_KIND, // 3 share label, 2 w/distinct labels
+    TWO_PAIR, // 2 share label, 2 share 2nd label
+    ONE_PAIR, // 2 share label, 3 w/distinct labels
+    HIGH_CARD, // all 5 have distinct labels
+  ];
+
+  const a_handTypeIndex = handType.indexOf(exports.getHandType(a.hand));
+  const b_handTypeIndex = handType.indexOf(exports.getHandType(b.hand));
+
+  if (a_handTypeIndex < b_handTypeIndex) {
+    return -1;
+  }
+  if (a_handTypeIndex > b_handTypeIndex) {
+    return 1;
+  }
+  if (a_handTypeIndex === b_handTypeIndex) {
+    // If two hands same type, secondary order rule:
+
+    // Card labels, strongest to weakest:
+    const cardLabel = [
+      'A',
+      'K',
+      'Q',
+      'J',
+      'T',
+      '9',
+      '8',
+      '7',
+      '6',
+      '5',
+      '4',
+      '3',
+      '2',
+    ];
+
+    // Comare cards in each hand, in order
+    // If they're different, the hand with the card with the stronger label is the stronger hand
+    // Otherwise move on to the next card in each hand
+    for (let i = 0; i < a.hand.length; i++) {
+      let a_charLabelIndex = cardLabel.indexOf(a.hand[i]);
+      let b_charLabelIndex = cardLabel.indexOf(b.hand[i]);
+
+      if (a_charLabelIndex < b_charLabelIndex) {
+        return -1;
+      }
+      if (a_charLabelIndex > b_charLabelIndex) {
+        return 1;
+      }
+    }
+  }
+  return 0;
+};
+
+exports.partOne = async (input) => {
+  // Goal to order hands based on the strength of each hand
+  // A hand consists of 5 cards
+  // each card has exactly one label, and each hand has exactly one type
+
+  // sort hands by hand strength
+  let sortedHands = input.sort(exports.compareHandStrengthPartOne);
+
+  // each hand wins an amount equal to bid multipled by its rank
+  // weakest hand is rank 1, second-weakest gets rank 2, etc.
+  let winnings = sortedHands.map((hand, index) => {
+    const rank = sortedHands.length - index;
+    return rank * hand.bid;
+  });
+
+  // what are the total winnings?
+  return sum(winnings);
+};
+
+// Part Two
+
+// J cards are jokers - wildcards that can act like whatever card would make the hand the strongest type possible
+// J cards are now the weakest individual cards
+const getDistinctAndCount = (str) => {
+  let m = new Map();
+  for (const char of str) {
+    if (!m.has(char)) {
+      const count = countOccurrence(char, str);
+      m.set(char, count);
+    }
+  }
+  return m;
+};
+exports.getHandTypeWithJokers = (hand) => {
+  const distinctChars = getDistinctAndCount(hand);
+  const charOccurrences = Array.from(distinctChars.values());
+
+  switch (distinctChars.size) {
+    case 1: // hand is 5:0 ratio
+      return FIVE_OF_A_KIND;
+    case 2: // hand is 4:1 or 3:2 ratio
+      if (distinctChars.has('J')) {
+        // if 4:1 && 4 or 1 Js => 5
+        // if 3:2 && 3 or 2 Js => 5
+        return FIVE_OF_A_KIND;
+      }
+
+      // 4:1 or 3:2
+      return charOccurrences.includes(4) ? FOUR_OF_A_KIND : FULL_HOUSE;
+
+    case 3: // hand is 3:1:1 or 2:2:1 ratio
+      if (distinctChars.has('J')) {
+        // if 3:1:1 && 3 or 1 Js => 4:1
+        if (charOccurrences.includes(3)) {
+          return FOUR_OF_A_KIND;
+        }
+
+        // if 2:2:1 && 2 Js => 4:1
+        if (distinctChars.get('J') === 2) {
+          return FOUR_OF_A_KIND;
+        }
+
+        // if 2:2:1 && 1 J => 3:2
+        return FULL_HOUSE;
+      }
+      // 3:1:1 or 2:2:1
+      return charOccurrences.includes(3) ? THREE_OF_A_KIND : TWO_PAIR;
+    case 4: // hand is 2:1:1:1
+      if (distinctChars.has('J')) {
+        // if 2:1:1:1 && 2 or 1 Js => 3:1:1
+        return THREE_OF_A_KIND;
+      }
+
+      // 2:1:1:1
+      return ONE_PAIR;
+
+    case 5: // hand is 1:1:1:1:1 ratio
+      if (distinctChars.has('J')) {
+        // if 1:1:1:1:1 && 1 J => 2:1:1:1
+        return ONE_PAIR;
+      }
+      return HIGH_CARD;
+  }
+};
+
+exports.compareHandStrengthWithJokers = (a, b) => {
+  // Order hands first by type
+
+  // Hand type, strongest to weakest:
+  const handType = [
+    FIVE_OF_A_KIND, // all 5 share label
+    FOUR_OF_A_KIND, // 4 share label, 1 w/distinct label
+    FULL_HOUSE, // 3 share label, 2 share 2nd label
+    THREE_OF_A_KIND, // 3 share label, 2 w/distinct labels
+    TWO_PAIR, // 2 share label, 2 share 2nd label
+    ONE_PAIR, // 2 share label, 3 w/distinct labels
+    HIGH_CARD, // all 5 have distinct labels
+  ];
+
+  const a_handTypeIndex = handType.indexOf(
+    exports.getHandTypeWithJokers(a.hand)
+  );
+  const b_handTypeIndex = handType.indexOf(
+    exports.getHandTypeWithJokers(b.hand)
+  );
+
+  if (a_handTypeIndex < b_handTypeIndex) {
+    return -1;
+  }
+  if (a_handTypeIndex > b_handTypeIndex) {
+    return 1;
+  }
+  if (a_handTypeIndex === b_handTypeIndex) {
+    // If two hands same type, secondary order rule:
+
+    // Card labels, strongest to weakest:
+    const cardLabel = [
+      'A',
+      'K',
+      'Q',
+      'T',
+      '9',
+      '8',
+      '7',
+      '6',
+      '5',
+      '4',
+      '3',
+      '2',
+      'J',
+    ];
+
+    // Comare cards in each hand, in order
+    // If they're different, the hand with the card with the stronger label is the stronger hand
+    // Otherwise move on to the next card in each hand
+    for (let i = 0; i < a.hand.length; i++) {
+      let a_charLabelIndex = cardLabel.indexOf(a.hand[i]);
+      let b_charLabelIndex = cardLabel.indexOf(b.hand[i]);
+
+      if (a_charLabelIndex < b_charLabelIndex) {
+        return -1;
+      }
+      if (a_charLabelIndex > b_charLabelIndex) {
+        return 1;
+      }
+    }
+  }
+  return 0;
+};
+exports.partTwo = async (input) => {
+  // Goal to order hands based on the strength of each hand
+  // A hand consists of 5 cards
+  // each card has exactly one label, and each hand has exactly one type
+
+  // sort hands by hand strength
+  let sortedHands = input.sort(exports.compareHandStrengthWithJokers);
+
+  // each hand wins an amount equal to bid multipled by its rank
+  // weakest hand is rank 1, second-weakest gets rank 2, etc.
+  let winnings = sortedHands.map((hand, index) => {
+    const rank = sortedHands.length - index;
+    return rank * hand.bid;
+  });
+
+  // what are the total winnings?
+  return sum(winnings);
+};
+
+exports.solve = async () => {
+  const dataPath = require.resolve(
+    '../../../src/AoC23/puzzleInputs/Day07Input.txt'
+  );
+
+  try {
+    const formattedData = await exports.formatData(dataPath);
+    const results = await Promise.all([
+      await exports.partOne(formattedData),
+      await exports.partTwo(formattedData),
+    ]);
+    console.log(results);
+    return results;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.solve();
---
+++ src/AoC23/Day07/Day07.spec.js
@@ -0,0 +1,242 @@
+const {
+  formatData,
+  getHandType,
+  compareHandStrengthPartOne,
+  partOne,
+  getHandTypeWithJokers,
+  compareHandStrengthWithJokers,
+  partTwo,
+} = require('./Day07');
+
+// npm test -- src/AoC23/Day07/Day07.spec.js
+
+describe('Day07', () => {
+  const mockData = [
+    {
+      hand: '32T3K',
+      bid: 765,
+    },
+    {
+      hand: 'T55J5',
+      bid: 684,
+    },
+    {
+      hand: 'KK677',
+      bid: 28,
+    },
+    {
+      hand: 'KTJJT',
+      bid: 220,
+    },
+    {
+      hand: 'QQQJA',
+      bid: 483,
+    },
+  ];
+  const FIVE_OF_A_KIND = 'FIVE_OF_A_KIND'; // all 5 share label
+  const FOUR_OF_A_KIND = 'FOUR_OF_A_KIND'; // 4 share label, 1 w/distinct label
+  const FULL_HOUSE = 'FULL_HOUSE'; // 3 share label, 2 share 2nd label
+  const THREE_OF_A_KIND = 'THREE_OF_A_KIND'; // 3 share label, 2 w/distinct labels
+  const TWO_PAIR = 'TWO_PAIR'; // 2 share label, 2 share 2nd label
+  const ONE_PAIR = 'ONE_PAIR'; // 2 share label, 3 w/distinct labels
+  const HIGH_CARD = 'HIGH_CARD'; // all 5 have distinct labels
+  describe('formatData', () => {
+    it('Formats the data into an array of objects', async () => {
+      const args = require.resolve('./Day07TestData.txt');
+      const actual = await formatData(args);
+      expect(actual).toEqual(mockData);
+    });
+  });
+  describe('getHandType', () => {
+    it('returns five of a kind', () => {
+      expect(getHandType('AAAAA')).toEqual(FIVE_OF_A_KIND);
+    });
+    it('returns four of a kind', () => {
+      expect(getHandType('AA8AA')).toEqual(FOUR_OF_A_KIND);
+    });
+    it('returns full house', () => {
+      expect(getHandType('23332')).toEqual(FULL_HOUSE);
+    });
+    it('returns three of a kind', () => {
+      expect(getHandType('TTT98')).toEqual(THREE_OF_A_KIND);
+    });
+    it('returns two pair', () => {
+      expect(getHandType('23432')).toEqual(TWO_PAIR);
+    });
+    it('returns one pair', () => {
+      expect(getHandType('A23A4')).toEqual(ONE_PAIR);
+    });
+    it('returns high card', () => {
+      expect(getHandType('23456')).toEqual(HIGH_CARD);
+    });
+  });
+  describe('compareHandStrengthPartOne', () => {
+    it('returns 1 when A is one pair and B is two pair', () => {
+      const a = {
+        hand: '32T3K',
+        bid: 765,
+      };
+      const b = {
+        hand: 'KK677',
+        bid: 28,
+      };
+      expect(compareHandStrengthPartOne(a, b)).toEqual(1);
+    });
+    it('returns 1 when A and B are both three of a kind but B is stronger', () => {
+      const b = {
+        hand: 'QQQJA',
+        bid: 483,
+      };
+      const a = {
+        hand: 'T55J5',
+        bid: 684,
+      };
+      expect(compareHandStrengthPartOne(a, b)).toEqual(1);
+    });
+    it('returns - 1 when A & B are both two pair but A is stronger', () => {
+      const a = {
+        hand: 'KK677',
+        bid: 28,
+      };
+      const b = {
+        hand: 'KTJJT',
+        bid: 220,
+      };
+      expect(compareHandStrengthPartOne(a, b)).toEqual(-1);
+    });
+    it('returns -1 when A is three of a kind and B is one pair', () => {
+      const a = {
+        hand: 'QQQJA',
+        bid: 483,
+      };
+      const b = {
+        hand: '32T3K',
+        bid: 765,
+      };
+      expect(compareHandStrengthPartOne(a, b)).toEqual(-1);
+    });
+    it('returns 0 when A and B are identical', () => {
+      const a = {
+        hand: 'T55J5',
+        bid: 684,
+      };
+      const b = {
+        hand: 'T55J5',
+        bid: 684,
+      };
+      expect(compareHandStrengthPartOne(a, b)).toEqual(0);
+    });
+  });
+  describe('partOne', () => {
+    it('Sum winnings', async () => {
+      const actual = await partOne(mockData);
+      expect(actual).toEqual(6440);
+    });
+  });
+  describe('getHandTypeWithJokers', () => {
+    it('returns five of a kind for 5:0 hand', () => {
+      expect(getHandTypeWithJokers('AAAAA')).toEqual(FIVE_OF_A_KIND);
+      expect(getHandTypeWithJokers('JJJJJ')).toEqual(FIVE_OF_A_KIND);
+    });
+    it('returns five of a kind for 4:1 ratio w/Js', () => {
+      expect(getHandTypeWithJokers('AAAAJ')).toEqual(FIVE_OF_A_KIND);
+      expect(getHandTypeWithJokers('JJJJA')).toEqual(FIVE_OF_A_KIND);
+    });
+    it('returns five of a kind for 3:2 ratio w/Js', () => {
+      expect(getHandTypeWithJokers('AAAJJ')).toEqual(FIVE_OF_A_KIND);
+      expect(getHandTypeWithJokers('JJJAA')).toEqual(FIVE_OF_A_KIND);
+    });
+    it('returns four of a kind for 3:1:1 ratio w/ Js', () => {
+      expect(getHandTypeWithJokers('AAAJ8')).toEqual(FOUR_OF_A_KIND);
+      expect(getHandTypeWithJokers('JJJA8')).toEqual(FOUR_OF_A_KIND);
+    });
+    it('returns four of a kind for 2:2:1 ratio w/ 2 Js', () => {
+      expect(getHandTypeWithJokers('JJAA8')).toEqual(FOUR_OF_A_KIND);
+    });
+    it('returns full house for 2:2:1 ratio w/ 1 J', () => {
+      expect(getHandTypeWithJokers('88AAJ')).toEqual(FULL_HOUSE);
+    });
+    it('returns three of a kind for 3:1:1 ratio w/o Js', () => {
+      expect(getHandTypeWithJokers('AAAT8')).toEqual(THREE_OF_A_KIND);
+    });
+    it('returns two pair for 2:2:1 ratio w/o Js', () => {
+      expect(getHandTypeWithJokers('TTAA8')).toEqual(TWO_PAIR);
+    });
+    it('returns three of a kind for 2:1:1:1 ratio w/ Js', () => {
+      expect(getHandTypeWithJokers('AAJT8')).toEqual(THREE_OF_A_KIND);
+      expect(getHandTypeWithJokers('JJAT8')).toEqual(THREE_OF_A_KIND);
+    });
+    it('returns one pair for 2:1:1:1 ratio w/o Js', () => {
+      expect(getHandTypeWithJokers('AA3T8')).toEqual(ONE_PAIR);
+    });
+    it('returns one pair for 1:1:1:1 ratio w/ Js', () => {
+      expect(getHandTypeWithJokers('JA3T8')).toEqual(ONE_PAIR);
+    });
+    it('returns high card for 1:1:1:1 ratio w/0 Js', () => {
+      expect(getHandTypeWithJokers('6A3T8')).toEqual(HIGH_CARD);
+    });
+  });
+  describe('compareHandStrengthWithJokers', () => {
+    it('returns 1 when A is two pair and B is full house w/joker', () => {
+      const a = {
+        hand: 'KK776',
+        bid: 28,
+      };
+      const b = {
+        hand: '23J32',
+        bid: 765,
+      };
+      expect(compareHandStrengthWithJokers(a, b)).toEqual(1);
+    });
+    it('returns 1 when A and B are both four of a kind but B is stronger', () => {
+      const b = {
+        hand: 'QQQJA',
+        bid: 483,
+      };
+      const a = {
+        hand: 'JJTTK',
+        bid: 220,
+      };
+      expect(compareHandStrengthWithJokers(a, b)).toEqual(1);
+    });
+    it('returns - 1 when A & B are both four of a kind but A is stronger', () => {
+      const a = {
+        hand: 'QQQJA',
+        bid: 483,
+      };
+      const b = {
+        hand: 'JJTTK',
+        bid: 220,
+      };
+      expect(compareHandStrengthWithJokers(a, b)).toEqual(-1);
+    });
+    it('returns -1 when A is three of a kind and B is two pair', () => {
+      const a = {
+        hand: 'QQDJA',
+        bid: 483,
+      };
+      const b = {
+        hand: 'KK677',
+        bid: 28,
+      };
+      expect(compareHandStrengthWithJokers(a, b)).toEqual(-1);
+    });
+    it('returns 0 when A and B are identical', () => {
+      const a = {
+        hand: 'T55J5',
+        bid: 684,
+      };
+      const b = {
+        hand: 'T55J5',
+        bid: 684,
+      };
+      expect(compareHandStrengthWithJokers(a, b)).toEqual(0);
+    });
+  });
+  describe('partTwo', () => {
+    it('Sum winnings with Jokers', async () => {
+      const actual = await partTwo(mockData);
+      expect(actual).toEqual(5905);
+    });
+  });
+});
