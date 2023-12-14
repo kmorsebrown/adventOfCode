d181989ea6e11936650b8937f7145406461c919c
Kelsey Morse-Brown <kelsey@jellyvision.com>
2023-12-14 01:47:41 -0600

    Day 10 part 2 unit tests

--- src/AoC23/Day10/Day10.spec.js
+++ src/AoC23/Day10/Day10.spec.js
@@ -14,6 +14,41 @@ const {
 describe('Day10', () => {
   const mockDataA = ['-L|F7', '7S-7|', 'L|7||', '-L-J|', 'L|-JF'];
   const mockDataB = ['7-F7-', '.FJ|7', 'SJLL7', '|F--J', 'LJ.LJ'];
+  const mockDataC = [
+    '...........',
+    '.S-------7.',
+    '.|F-----7|.',
+    '.||.....||.',
+    '.||.....||.',
+    '.|L-7.F-J|.',
+    '.|..|.|..|.',
+    '.L--J.L--J.',
+    '...........',
+  ];
+  const mockDataD = [
+    '.F----7F7F7F7F-7....',
+    '.|F--7||||||||FJ....',
+    '.||.FJ||||||||L7....',
+    'FJL7L7LJLJ||LJ.L-7..',
+    'L--J.L7...LJS7F-7L7.',
+    '....F-J..F7FJ|L7L7L7',
+    '....L7.F7||L7|.L7L7|',
+    '.....|FJLJ|FJ|F7|.LJ',
+    '....FJL-7.||.||||...',
+    '....L---J.LJ.LJLJ...',
+  ];
+  const mockDataE = [
+    'FF7FSF7F7F7F7F7F---7',
+    'L|LJ||||||||||||F--J',
+    'FL-7LJLJ||||||LJL-77',
+    'F--JF--7||LJLJ7F7FJ-',
+    'L---JF-JLJ.||-FJLJJ7',
+    '|F|F-JF---7F7-L7L|7|',
+    '|FFJF7L7F-JF7|JL---7',
+    '7-L-JL7||F7|L7F-7F7|',
+    'L.L7LFJ|||||FJL7||LJ',
+    'L7JLJL-JLJLJL--JLJ.L',
+  ];
   const NORTH = 'NORTH';
   const SOUTH = 'SOUTH';
   const EAST = 'EAST';
@@ -211,10 +246,17 @@ describe('Day10', () => {
     });
   });
   describe.skip('partTwo', () => {
-    it('TK', async () => {
-      const args = [];
-      const actual = await partTwo(args);
-      expect(actual).toEqual(0);
+    it('Returns the number of tiles enclosed by the loop for grid C', async () => {
+      const actual = await partTwo(mockDataC);
+      expect(actual).toEqual(4);
+    });
+    it('Returns the number of tiles enclosed by the loop for grid D', async () => {
+      const actual = await partTwo(mockDataD);
+      expect(actual).toEqual(8);
+    });
+    it('Returns the number of tiles enclosed by the loop for grid E', async () => {
+      const actual = await partTwo(mockDataE);
+      expect(actual).toEqual(10);
     });
   });
 });
