98104b27fc7d6c072f80e15f14da2f5996202b68
Kelsey Morse-Brown <132404623+kelseymorsebrown@users.noreply.github.com>
2024-12-13 17:54:25 -0600

    AoC 24 Day 11 Part 1 solve

--- src/AoC24/Day11/Day11.js
+++ src/AoC24/Day11/Day11.js
@@ -12,17 +12,84 @@ exports.formatData = async (filepath) => {
 
 /**
  *
- * @param {string[]} input
+ * @param {string[]} stones
  */
-exports.transformStones = (input) => {};
+exports.transformStones = (stones) => {
+  const queue = new Queue();
+  const transformedStones = [];
+  for (let i = 0; i < stones.length; i++) {
+    queue.enqueue(stones[i]);
+  }
 
-exports.partOne = async (input) => {
-  return input;
+  while (!queue.isEmpty()) {
+    const stone = queue.front();
+    let transformedStone = [];
+
+    if (stone === '0') {
+      // If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
+      transformedStone.push('1');
+    } else if (stone.length % 2 === 0) {
+      // If the stone is engraved with a number that has an even number of digits, it is replaced by two stones.
+
+      // The left half of the digits are engraved on the new left stone
+      const leftHalf = stone.slice(0, stone.length / 2);
+
+      // The right half of the digits are engraved on the new right stone.
+      const rightHalf = stone.slice(stone.length / 2, stone.length);
+
+      // The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+      transformedStone.push(`${parseInt(leftHalf, 10)}`);
+      transformedStone.push(`${parseInt(rightHalf, 10)}`);
+    } else {
+      // If none of the other rules apply, the stone is replaced by a new stone
+      // the old stone's number multiplied by 2024 is engraved on the new stone.
+      transformedStone.push(`${parseInt(stone, 10) * 2024}`);
+    }
+
+    transformedStones.push(...transformedStone);
+    queue.dequeue();
+  }
+
+  return transformedStones;
+};
+
+exports.blink = (input, blinks) => {
+  const queue = new Queue();
+  let blink = 0;
+  let result;
+
+  queue.enqueue(input);
+
+  while (!queue.isEmpty()) {
+    blink += 1;
+    const stones = queue.front();
+    const transformedStones = exports.transformStones(stones);
+    console.log(`blink ${blink}`);
+    if (blink < blinks) {
+      queue.enqueue(transformedStones);
+    } else {
+      result = transformedStones;
+    }
+    queue.dequeue();
+  }
+  return result;
+};
+
+exports.partOne = async (input, blinks) => {
+  const transformedStones = exports.blink(input, blinks);
+  return transformedStones.length;
 };
 
 // Part Two
-exports.partTwo = async (input) => {
-  return input;
+
+exports.partTwo = async (input, blinks) => {
+  let numStones = 0;
+  for (let i = 0; i < input.length; i++) {
+    console.log(`Stone ${i}`);
+    numStones += exports.blink([input[i]], blinks).length;
+    console.log(`After stone ${i} > ${numStones} stones`);
+  }
+  return numStones;
 };
 
 exports.solve = async () => {
@@ -33,8 +100,8 @@ exports.solve = async () => {
   try {
     const formattedData = await exports.formatData(dataPath);
     const results = await Promise.all([
-      await exports.partOne(formattedData),
-      await exports.partTwo(formattedData),
+      // await exports.partOne(formattedData, 25),
+      await exports.partTwo(formattedData, 75),
     ]);
     console.log(results);
     return results;
--- src/AoC24/Day11/Day11.spec.js
+++ src/AoC24/Day11/Day11.spec.js
@@ -1,9 +1,16 @@
-const { formatData, partOne, partTwo } = require('./Day11');
+const {
+  formatData,
+  transformStones,
+  blink,
+  partOne,
+  partTwo,
+} = require('./Day11');
 
 // npm test -- src/AoC24/Day11/Day11.spec.js
 
 describe('Day11', () => {
   const mockInput = ['0', '1', '10', '99', '999'];
+  const mockInputTwo = ['125', '17'];
   describe('formatData', () => {
     it('Formats the data into an array', async () => {
       const args = require.resolve('./Day11TestData.txt');
@@ -11,18 +18,63 @@ describe('Day11', () => {
       expect(actual).toEqual(mockInput);
     });
   });
-  describe.skip('partOne', () => {
-    it('TK', async () => {
-      const args = [];
-      const actual = await partOne(args);
-      expect(actual).toEqual(0);
+  describe('transformStones', () => {
+    it('transforms stones according to the rules', () => {
+      const actual = transformStones(mockInput);
+      expect(actual).toEqual(['1', '2024', '1', '0', '9', '9', '2021976']);
+    });
+    it('handles leading zeroes', () => {
+      const actual = transformStones(['1000', '100000']);
+      expect(actual).toEqual(['10', '0', '100', '0']);
     });
   });
-  describe.skip('partTwo', () => {
-    it('TK', async () => {
-      const args = [];
-      const actual = await partTwo(args);
-      expect(actual).toEqual(0);
+  describe('blink', () => {
+    it('transforms stones after 3 blinks', () => {
+      const actual = blink(mockInputTwo, 3);
+      expect(actual).toEqual(['512072', '1', '20', '24', '28676032']);
+    });
+    it('transforms stones after 6 blinks', () => {
+      const actual = blink(mockInputTwo, 6);
+      expect(actual).toEqual([
+        '2097446912',
+        '14168',
+        '4048',
+        '2',
+        '0',
+        '2',
+        '4',
+        '40',
+        '48',
+        '2024',
+        '40',
+        '48',
+        '80',
+        '96',
+        '2',
+        '8',
+        '6',
+        '7',
+        '6',
+        '0',
+        '3',
+        '2',
+      ]);
+    });
+  });
+  describe('partOne', () => {
+    it('Returns the number of stones after 6 blinks', async () => {
+      const actual = await partOne(mockInputTwo, 6);
+      expect(actual).toEqual(22);
+    });
+    it('Returns the number of stones after 25 blinks', async () => {
+      const actual = await partOne(mockInputTwo, 25);
+      expect(actual).toEqual(55312);
+    });
+  });
+  describe('partTwo', () => {
+    it('Returns the number of stones after 25 blinks', async () => {
+      const actual = await partTwo(mockInputTwo, 25);
+      expect(actual).toEqual(55312);
     });
   });
 });
