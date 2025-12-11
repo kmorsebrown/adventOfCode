3d2a8dc34bbef1ebd7863be1a84b87bcc483c0af
Kelsey Morse-Brown <kelsey@artsortment.me>
2025-12-11 13:36:35 -0600

    Part 1

---
+++ src/AoC25/Day05/Day05.spec.js
@@ -0,0 +1,35 @@
+const { formatData, partOne, partTwo } = require('./Day05');
+
+// npm test -- src/AoC25/Day05/Day05.spec.js
+
+describe('Day05', () => {
+  const mockInput = {
+    freshIds: [
+      [3, 5],
+      [10, 14],
+      [16, 20],
+      [12, 18],
+    ],
+    availableIds: [1, 5, 8, 11, 17, 32],
+  };
+  describe('formatData', () => {
+    it('Formats the data into an array', async () => {
+      const args = require.resolve('./Day05TestData.txt');
+      const actual = await formatData(args);
+      expect(actual).toEqual(mockInput);
+    });
+  });
+  describe('partOne', () => {
+    it('Returns the number of fresh available ingredients', async () => {
+      const actual = await partOne(mockInput);
+      expect(actual).toEqual(3);
+    });
+  });
+  describe.skip('partTwo', () => {
+    it('TK', async () => {
+      const args = [];
+      const actual = await partTwo(args);
+      expect(actual).toEqual(0);
+    });
+  });
+});
--- src/Utils/parse.js
+++ src/Utils/parse.js
@@ -9,3 +9,28 @@ exports.parseStringOfInts = (str, separator) => {
 exports.unique = (arr) => {
   return arr.filter((value, index, array) => array.indexOf(value) === index);
 };
+
+// https://www.geeksforgeeks.org/dsa/merging-intervals/
+exports.mergeOverlap = (arr) => {
+  if (arr.length === 0) return [];
+
+  // Sort intervals based on start values
+  const sorted = [...arr].sort((a, b) => a[0] - b[0]);
+
+  const res = [];
+  res.push(sorted[0]);
+
+  for (let i = 1; i < sorted.length; i++) {
+    const last = res[res.length - 1];
+    const curr = sorted[i];
+
+    // If current interval overlaps with last, merge them
+    if (curr[0] <= last[1]) {
+      last[1] = Math.max(last[1], curr[1]);
+    } else {
+      res.push(curr);
+    }
+  }
+
+  return res;
+};
